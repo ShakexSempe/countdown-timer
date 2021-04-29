84420f4d73094467310053a4b7d28e3a5deb5ca1
Ray-Shakex <bshakex@gmail.com>
2021-04-29 11:34:04 +0200

    getReaminingTime variable

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ month = futureDate.getMonth();
 date = futureDate.getDate();
 weekday = futureDate.getDay() = returns a value[0-6],
     
+
+## Countdown timer
+divide time remaining by the value of miliseconds in a day to know how many days the 'time' value represents 
+### values in miliseconds
+ 1s = 1000ms
+ 1m = 60s
+ 1hr = 60min
+ 1d = 24hr.
+
+1 day = 24*60*60*1000 = 86400000; 
--- app.js
+++ app.js
@@ -27,7 +27,8 @@ const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 let futureDate = new Date(2021, 5, 8, 12, 30, 30, 500);
-console.log(futureDate);
+
+
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -38,7 +39,28 @@ month = months[month];
 
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
-console.log(weekday);
 
 //display on DOM: 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year}, ${hours}:${mins}pm`
+
+//future time in ms 
+const futureTime = futureDate.getTime();
+
+
+getRemainingTime = () => {
+  const today = new Date().getTime();
+  const time = futureTime - today;
+  
+  //values in miliseconds
+  //24hr * 60m * 60s *1000ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000; 
+  //calculate all values 
+  let days = time / oneDay;
+  days = Math.floor(days);
+  let hours = time / oneHour;
+  
+}
+
+getRemainingTime();
